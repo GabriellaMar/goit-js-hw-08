a783536f14672c257e5b5b53f416497abf0e3676
Gabriella Marusyak <slipuhinag@gmail.com>
2023-04-26 13:45:58 +0300

    Added Vimeo Player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let currentTime = 0;
+player.setCurrentTime(currentTime);
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+    currentTime = data.seconds;
+       
+  
+};
+
+player.on('play', onPlay);
