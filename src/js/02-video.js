eec9f3d82c131d52caf83dca1cf8560ccb096f16
Gabriella Marusyak <slipuhinag@gmail.com>
2023-04-26 16:50:35 +0300

    Created form-submit task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,32 @@
+const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle'
 import Player from '@vimeo/player';
 
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-let currentTime = 0;
+const timeKey = "videoplayer-current-time";
+
+const onTimeUpdate = throttle(function (data) {
+    localStorage.setItem(timeKey, data.seconds);
+}, 1000);
+
+player.on('timeupdate', onTimeUpdate);
+
+
+
+let currentTime = parseFloat(localStorage.getItem(timeKey))
+console.log(currentTime)
+
 player.setCurrentTime(currentTime);
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-    currentTime = data.seconds;
-       
-  
-};
 
-player.on('play', onPlay);
+
+
+
+//   const onPlay = function(data) {
+//      currentTime = data.seconds;
+//       console.log('Current time is ', currentTime);
+//     //  localStorage.setItem(timeKey, currentTime);
+//   };
+//  player.on("timeupdate", onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+ throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle'
 
+const feadBackForm = document.querySelector('.feedback-form')
+// console.log(feadBackForm)
+const emailInput = document.querySelector('input')
+// console.log(emailInput)
+const messageArea = document.querySelector('textarea')
+// console.log(messageArea)
+
+const emailEl = emailInput.addEventListener('input', onEmailInput)
+
+function onEmailInput(e) {
+    console.log(e.target.value)
+}
+const messageEl = messageArea.addEventListener('input', onEmailInput)
+
+const formSubmit = feadBackForm.addEventListener("submit", onFormSubmit)
+
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+
+function onFormSubmit(event) {
+
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    console.log(formElements)
+
+    const mail = formElements.email.value;
+
+    const message = formElements.message.value;
+    
+    let formData = {};
+
+    if (mail !== '' && message !== '') {
+
+        const formData = {
+            email: mail,
+            message: message
+        }
+        console.log(formData)
+
+        // formEl.reset();
+    }
+    else {
+        alert('Всі поля повинні бути заповнені.')
+    }
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData))
+}
