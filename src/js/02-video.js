710a250389d00b2bda432fc993461f8cb45f7cd5
Gabriella Marusyak <slipuhinag@gmail.com>
2023-04-27 11:55:00 +0300

    did task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,32 +2,19 @@ const throttle = require('lodash.throttle');
 import throttle from 'lodash.throttle'
 import Player from '@vimeo/player';
 
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const timeKey = "videoplayer-current-time";
+
+const STORAGETIME_KEY = "videoplayer-current-time";
 
 const onTimeUpdate = throttle(function (data) {
-    localStorage.setItem(timeKey, data.seconds);
+    localStorage.setItem(STORAGETIME_KEY, data.seconds);
     // console.log(data.seconds)
 }, 1000);
 
 player.on('timeupdate', onTimeUpdate);
 
-
-
-let currentTime = parseFloat(localStorage.getItem(timeKey))
+let currentTime = parseFloat(localStorage.getItem(STORAGETIME_KEY))
 console.log(currentTime)
 
 player.setCurrentTime(currentTime);
-
-
-
-
-
-//   const onPlay = function(data) {
-//      currentTime = data.seconds;
-//       console.log('Current time is ', currentTime);
-//     //  localStorage.setItem(timeKey, currentTime);
-//   };
-//  player.on("timeupdate", onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,87 +1,28 @@
- throttle = require('lodash.throttle');
 import throttle from 'lodash.throttle'
 
-// const feadBackForm = document.querySelector('.feedback-form')
-// // console.log(feadBackForm)
-// const emailInput = document.querySelector('input')
-// // console.log(emailInput)
-// const messageArea = document.querySelector('textarea')
-// // console.log(messageArea)
-
-//  emailInput.addEventListener('input', onInput)
-
-//  messageArea.addEventListener('input', onInput)
-
-//  feadBackForm.addEventListener("submit", onFormSubmit)
-
-// const LOCALSTORAGE_KEY = "feedback-form-state";
-
-// function onInput(e) {
-//     console.log(e.target.value)
-//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-//     // e.target.value = localStorage.getItem(LOCALSTORAGE_KEY) || ""
-// }
-
-// function resetForm() {
-//     feedbackForm.reset();
-//     localStorage.removeItem(LOCALSTORAGE_KEY);
-//   }
-
-
-// function onFormSubmit(event) {
-
-//     event.preventDefault();
-
-//     // const formElements = event.currentTarget.elements;
-//     // console.log(formElements)
-
-//     const mail = emailInput.value;
-//   const message = messageArea.value;
-    
-
-//         const formData = {
-//             email: mail,
-//             message: message
-//         }
-//         console.log(formData)
-
-//         resetForm();
-    
-
-  
-//     // updateOutput();
-// }
-
-// function updateOutput() {
-//     const savedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-//     if (savedValue) {
-//       emailInput.value = savedValue.email;
-//       messageArea.value = savedValue.message;
-//     } else {
-//       emailInput.value = '';
-//       messageArea.value = '';
-//     }
-//   }
-//   updateOutput()
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('input');
 const messageArea = document.querySelector('textarea');
+
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
 function onInput(e) {
-   const formData = {
-     email: emailInput.value,
-     message: messageArea.value
-   };
+  const formData = {
+    email: emailInput.value,
+    message: messageArea.value
+  };
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 
-function resetForm() {
-  feedbackForm.reset();
-  localStorage.removeItem(LOCALSTORAGE_KEY);
-}
+const onThrottleInput = throttle(onInput, 500);
 
-function  onFormSubmit() {
+emailInput.addEventListener('input', onThrottleInput);
+messageArea.addEventListener('input', onThrottleInput);
+feedbackForm.addEventListener("submit", onFormSubmit)
+
+populateInputs();
+
+function populateInputs() {
   const savedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
   if (savedValue) {
     emailInput.value = savedValue.email;
@@ -89,19 +30,18 @@ function  onFormSubmit() {
   }
 }
 
-feedbackForm.addEventListener("submit", (event) => {
+function onFormSubmit(event)  {
   event.preventDefault();
   const mail = emailInput.value;
   const message = messageArea.value;
+
   const formData = {
     email: mail,
     message: message
   };
   console.log(formData);
-  resetForm();
-});
+  feedbackForm.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+};
 
-emailInput.addEventListener('input', onInput);
-messageArea.addEventListener('input', onInput);
 
-onFormSubmit();
