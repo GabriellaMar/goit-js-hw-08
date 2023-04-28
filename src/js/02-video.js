ce05e1f8f9ba27761cc280beaa89ddf3dbfa2f92
Gabriella Marusyak <slipuhinag@gmail.com>
2023-04-28 14:34:21 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,21 +2,28 @@
 import throttle from 'lodash.throttle'
 import Player from '@vimeo/player';
 
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const STORAGETIME_KEY = "videoplayer-current-time";
 
 const onTimeUpdate = (data) => {
-    localStorage.setItem(STORAGETIME_KEY, data.seconds);
-    // console.log(data.seconds)
+  localStorage.setItem(STORAGETIME_KEY, data.seconds);
 };
-
 const throttledOnTimeUpdate = throttle(onTimeUpdate, 1000);
 
 player.on('timeupdate', throttledOnTimeUpdate);
 
-let currentTime = parseFloat(localStorage.getItem(STORAGETIME_KEY))
-console.log(currentTime)
+const videoCurrentTime = localStorage.getItem(STORAGETIME_KEY);
 
-player.setCurrentTime(currentTime);
+if (videoCurrentTime >= 0 && videoCurrentTime < player.getDuration()) {
+  player.setCurrentTime(videoCurrentTime)
+  
+} else {
+  player.setCurrentTime(0)
+  console.log(videoCurrentTime)
+  localStorage.removeItem(STORAGETIME_KEY);
+
+}
+
